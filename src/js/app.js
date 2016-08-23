e9da5fb3fd592e473bde51f3e700674084e68a63
SteveCarrasco <casteve@wanadoo.fr>
2016-08-23 16:19:50 +0200

    module view inscription & view produit

--- src/js/app.js
+++ src/js/app.js
@@ -6,14 +6,18 @@ import Wrapper from './components/Wrapper'
 import Dashboard from './components/Dashboard'
 import Home from './components/Home'
 import Test from './containers/test'
+import Produit from './components/Produit'
+import Inscription from './components/Inscription'
 
 const App = ({ history }) => (
   <Router history={history}>
     <Route path='/'>
+     <IndexRoute component={Home}/>
       <Route component={Wrapper}>
-        <IndexRoute component={Home}/>
         <Route path='test' component={Test}/>
         <Route path='dashboard' component={Dashboard}/>
+        <Route path='produits' component={Produit}/>
+        <Route path='inscription' component={Inscription}/>
       </Route>
     </Route>
   </Router>
